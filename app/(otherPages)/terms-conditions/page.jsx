3a15eb179e01f0bdab68300a8d4ae2b0e10c95c3
miknivin <116654570+miknivin@users.noreply.github.com>
2026-02-25 12:46:39 +0530

    legal pages added-3

--- app/(otherPages)/terms-conditions/page.jsx
+++ app/(otherPages)/terms-conditions/page.jsx
@@ -1,6 +1,5 @@
 import Footer1 from "@/components/footers/Footer1";
 import Header2 from "@/components/headers/Header2";
-import Header2 from "@/components/headers/Header2";
 import Link from "next/link";
 import React from "react";
 
